cb33cb82be1bc30407b77622770d46f63ff82a1b
Ivan Frolov <Frolls@outlook.com>
2022-05-26 17:13:20 +0500

    add 05-virt-04-docker-compose

---
+++ 05-virt-04-docker-compose/src/task2/terraform/variables.tf
@@ -0,0 +1,17 @@
+# Заменить на ID своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_cloud_id" {
+  default = "b1gkvhe2rk0g1fi0uu3t"
+}
+
+# Заменить на Folder своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_folder_id" {
+  default = "b1gav9nslcgv47g7gpim"
+}
+
+# Заменить на ID своего образа
+# ID можно узнать с помощью команды yc compute image list
+variable "centos-7-base" {
+  default = "fd8q58cjn90u230favc5"
+}
