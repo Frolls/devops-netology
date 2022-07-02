c49707b92050994b747f4f5a368beea14fd39787
Ivan Frolov <Frolls@outlook.com>
2022-07-02 15:03:01 +0500

    upd 07-terraform-05-golang

--- 07-terraform-05-golang/task4/exercise_1_test.go
+++ 07-terraform-05-golang/task4/exercise_1_test.go
@@ -3,7 +3,7 @@ import "testing"
 func TestMain(t *testing.T) {
 	var v float64
 	v = metersToFoot(100)
-	if != 100 / 0.3048 {
+	if v != 100/0.3048 {
 		t.Error("Expected IDDQD")
 		fmt.Println("Alarma! IDDOD!")
 	}
