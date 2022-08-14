0a7c6ef3467faf99498921f08a0ec709aadacd0b
Ivan Frolov <Frolls@outlook.com>
2022-08-14 16:37:43 +0500

    upd 09-ci-02-cicd

--- 09-ci-02-cicd/example/fail.py
+++ 09-ci-02-cicd/example/fail.py
@@ -1,11 +1,15 @@
 def increment(index):
-    index =+ 1
+    index += 1
     return index
+
+
 def get_square(numb):
     return numb*numb
+
+
 def print_numb(numb):
     print("Number is {}".format(numb))
-    pass
+
 
 index = 0
 while (index < 10):
